95a47e5dd1b411b2e420798c75caf5457f0936e4
Luckie Bah <bah966@gmail.com>
2023-04-01 12:17:39 +0100

    finished the table and then started on script first set the ready command and made a on click for the submit button then created a function to correspond that appended the values of the employees info onto the tbody section currently working on delete button and then calculator

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+
 console.log('You got this');
+// code stating that when the document is ready to 
+// run the onready code
+$(document).ready(onReady);
+
+// function that will serve as a staging area for events
+function onReady(){
+    // function that says once the submit button is clicked
+    // to run the insert employee function
+ $('.submit-button').on('click',insertEmployee)
+}
+
+function insertEmployee(event){
+    event.preventDefault();
+    if ($('#firstName').val() !='' && $('#id').val()!=''){
+        let employee= {
+            firstName:$('#firstName').val(),
+            lastName: $('#lastName').val(),
+            id: $('#id').val(),
+            Title: $('#title').val(),
+            annualSalary: $('#annualSalary').val()
+        }
+        $('#tbody').append(`<tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.id}</td>
+        <td>${employee.Title}</td>
+        <td>${employee.annualSalary}</td>
+        <td>
+          <button class="deleteButton">Delete</button>
+        </td>
+    </tr>`)
+        
+    $('#firstName').val('')
+    $('#lastName').val('')
+    $('#id').val('')
+    $('#title').val('')
+    $('#annualSalary').val('')
+    }
+}
